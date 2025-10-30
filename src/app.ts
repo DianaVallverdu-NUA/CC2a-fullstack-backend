8086e5779437f2d012179178afa2af1d7e579ca6
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-10-30 06:31:50 +0000

    get request for counter

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
 
+let counter = 0; 
+
 // create express app & choose port
 const app = express();
 const port = 3000;
@@ -16,7 +18,13 @@ app.get('/', (req, res) => {
   res.send({ message: 'Hello World!' })
 })
 
+// counter
+app.get('/counter', (req, res) => {
+  res.send({counter: counter});
+})
+
 // start server
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
