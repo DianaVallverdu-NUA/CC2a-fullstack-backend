714c18308b46a65e34b92d0788c203b74c6c17ed
DVallverdu-NUA <d.vallverdu@norwichuni.ac.uk>
2025-12-01 20:45:08 +0000

    reset cors

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,18 @@
 import express from "express";
-// import cors from "cors";
+import cors from "cors";
 import bodyParser from "body-parser";
 
+let counter = 0;
 
 // create express app & choose port
 const app = express();
 const port = 3000;
 
-// app.use(cors({
-//   origin: "https://cc-2a-fullstack-frontend.vercel.app",
-//   methods: "GET,POST,OPTIONS",
-//   allowedHeaders: "Content-Type",
-// }));
-
-// app.options("*", (req, res) => {
-//   res.sendStatus(200);
-// });
-
-let counter = 0;
-
 // setup CORS to allow requests from any origin
-// const corsOptions = {
-//   origin: '*'
-// }
-// app.use(cors(corsOptions));
+const corsOptions = {
+  origin: '*'
+}
+app.use(cors(corsOptions));
 
 // setup JSON & body Parser
 app.use(bodyParser.json()); // parse json requests
