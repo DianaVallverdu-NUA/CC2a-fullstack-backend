1cdc3545ff739510874e89dfa23d245b6afa41a8
DianaVallverdu-NUA <148062880+DianaVallverdu-NUA@users.noreply.github.com>
2026-09-04 15:11:14 +0100

    starting point

--- src/index.ts
+++ src/index.ts
@@ -6,24 +6,8 @@
  */
 
 import express, { type Express, type Request, type Response } from "express";
-import { pictures, placeholder } from "./pictures.ts";
 
 const app: Express = express();
-
-app.use(express.static("assets"));
-
-app.get("/pictures", (_req: Request, res: Response) => {
-  res.send(pictures);
-});
-
-app.get("/placeholder", (_req: Request, res: Response) => {
-  res.send(placeholder);
-});
-
-app.post("/picture", (req: Request, res: Response) => {
-  console.log("received image: ", req);
-  res.send("received image");
-});
 
 app.get("/", (_req: Request, res: Response) => {
   res.send("Hello World!");
--- src/types.d.ts
+++
@@ -1,6 +0,0 @@
-type Picture = {
-    id: string,
-    heading: string,
-    description: string,
-    type?: 'jpeg' | 'png'
-}
